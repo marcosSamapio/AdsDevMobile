a0506831570ca631e1138732637e747d50d690c8
marcosSampaio <vinicius.marcos.mvvlk@gmail.com>
2020-03-03 01:51:15 -0300

    Adicionado metodo subtracao

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -9,12 +9,22 @@ public class Calculadora {
 		double resultado = 0;
 		
 		resultado = somar(valorA, valorB);
+		System.out.println("A somatória dos números é: " + resultado);
 		
-		System.out.println("A somatória dos números é: " + resultado);		
+		resultado = subtrair(valorA, valorB);
+		System.out.println("A subtracao dos números é: " + resultado);
 	}
 
 	public static int somar(int valorA, int valorB) {
 		return valorA + valorB;
+	}
+	
+	public static int subtrair(int valorA, int valorB) {
+		if (valorA >= valorB) {
+			return valorA - valorB;
+		} else {
+			return valorB - valorA;
+		}
 	}
 	
 	public static int parseInt(String valor) {
